d062f66a2b23adadc488ca173d2408278b9e063b
AlanCordero <156855819+AlanCordero@users.noreply.github.com>
2024-11-01 10:34:37 -0600

    Dispet

---
+++ Dispet/Dispet/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Dispet.Models // Asegúrate de que el namespace corresponda a tu proyecto
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
